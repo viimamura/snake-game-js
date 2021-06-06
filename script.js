e15a47ff153f07acecba12bc11f6ca63c4987f26
vitoriasue <vitoria.imamura@hotmail.com>
2021-06-05 23:39:20 -0300

    define game over

--- script.js
+++ script.js
@@ -45,6 +45,14 @@ function startGame(){
     if(snake[0].y > 15 * box && direction =="down") snake[0].y = 0;
     if(snake[0].y < 15 && direction =="up") snake[0].y = 16 * box;
 
+    //verify game over
+    for(i=1; i<snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(game);
+            alert("Game Over.");
+        }
+    }
+
     createBG();
     createSnake();
     showFood();
